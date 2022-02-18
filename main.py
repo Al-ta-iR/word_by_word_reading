18872d3ffdcb4289da4d1da88bf13e76319a3e88
Artyom Oleynik <artyom.oleynik@gmail.com>
2022-02-18 20:52:32 +0300

    form_1

---
+++ main.py
@@ -0,0 +1,21 @@
+import eel
+
+
+eel.init('web')
+eel.start('index.html', size = (600, 600))
+
+@eel.expose
+def call_in_js(x):
+    print(x)
+
+# eel.call_in_python("Hello from JS")
+
+# форма
+# скорость
+# экран и размер экрана
+# открытие файла
+# размер шрифта
+# цвета фона и шрифта
+# прогресс
+# пауза/плей
+# запуск с прогресса
