8c1c43c9ee6cc050968e6311cdcd3880f471e52f
Artyom Oleynik <artyom.oleynik@gmail.com>
2022-02-19 01:20:04 +0300

    logic-1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import eel
+import time
 
 
 eel.init('web')
@@ -7,6 +8,21 @@ eel.start('index.html', size = (750, 600))
 @eel.expose
 def call_in_js(x):
     print(x)
+
+file = open('text.txt', encoding="utf-8")
+speed = int(input())
+read_data = file.read()
+per_word = read_data.split()
+count_words = len(read_data.split())
+progress = 0
+count_read_words = 0
+for word in per_word:
+    # for word in line.split():
+    print(word, sep='')
+    count_read_words += 1
+    # print('Прочитано = ', count_read_words, end='')
+    progress = count_read_words * 100 / 50
+    time.sleep(60 / speed)
 
 # eel.call_in_python("Hello from JS")
 
