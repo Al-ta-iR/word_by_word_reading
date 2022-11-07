b02eb299c3e0510e86d5b1f9acc66e7d5be2e88c
Al-ta-iR <artyom.oleynik@gmail.com>
2022-11-07 10:55:32 +0100

    add messaging functional

--- mai.py
+++ mai.py
@@ -7,7 +7,10 @@ eel.init('web')
 @eel.expose
 def call_in_js(x):
     print('123')
+    print(x)
     return '257'
+
+eel.call_in_python("Hello, from JS")
 
 eel.start('ind.html', size = (750, 600), mode = "firefox")
 
